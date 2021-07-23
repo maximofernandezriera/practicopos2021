812856e623106b8c2faf87f59225bef745c43c84
maximo <maxfernandez@gmail.com>
2021-07-23 13:33:25 +0200

    Code improvement. Main if instead a second while loop

--- breadth-first_search.py
+++ breadth-first_search.py
@@ -10,26 +10,26 @@ graph_following_users = {}
 graph_following_users["maximofdezriera"] = ["dfleta", "classicoman2", "jbarcelo"]
 graph_following_users["classicoman2"] = ["miquelcabot$", "jaumerp"]
 graph_following_users["dfleta"] = ["jaumerp"]
-graph_following_users["jbarcelo"] = ["jaumeol", "albertosoto$"]
+graph_following_users["jbarcelo"] = ["jaumeol$", "albertosoto$"]
 graph_following_users["miquelcabot$"] = []
 graph_following_users["jaumerp"] = []
-graph_following_users["jaumeol"] = []
+graph_following_users["jaumeol$"] = []
 graph_following_users["albertosoto$"] = []
 
 
 def search(name):
     search_queue = deque()
     search_queue += graph_following_users[name]
-    # This is how you keep track of which people you've searched before
+    # This is how you keep track of which user you've searched before
     searched = set()
     # Check if some user is a python developer
     checker = False
     while search_queue:
         github_user = search_queue.popleft()
-        # Only search this person if you haven't already searched them
+        # Only search this user if you haven't already searched them
         nobreak = True
         # Using a boolean in order to avoid infinite loops
-        while github_user not in searched and nobreak:
+        if github_user not in searched and nobreak:
             if is_python_developer(github_user) and nobreak:
                 print(github_user + " is a python developer!")
                 checker = True
